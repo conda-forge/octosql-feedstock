1dca1257f47460cf99f4cade0b58866c8972641a
danielnachun <daniel.nachun@gmail.com>
2024-07-22 11:57:15 -0700

    enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X github.com/cube2222/octosql/cmd.VERSION=${PKG_VERSION}"
 go-licenses save . --save_path=license-files --ignore github.com/cube2222/octosql --ignore github.com/xi2/xz
 
-mkdir -p ${PREFIX}/etc/bash_completion.d
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d
-mkdir -p ${PREFIX}/share/zsh/site-functions
-octosql completion bash > ${PREFIX}/etc/bash_completion.d/octosql
-octosql completion fish > ${PREFIX}/share/fish/vendor_completions.d/octosql.fish
-octosql completion zsh > ${PREFIX}/share/zsh/site-functions/_octosql
+if [[ ${build_platform} == ${target_platform ]]; then
+    mkdir -p ${PREFIX}/etc/bash_completion.d
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    octosql completion bash > ${PREFIX}/etc/bash_completion.d/octosql
+    octosql completion fish > ${PREFIX}/share/fish/vendor_completions.d/octosql.fish
+    octosql completion zsh > ${PREFIX}/share/zsh/site-functions/_octosql
+fi
